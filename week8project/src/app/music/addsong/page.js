8fcda6eb4a2ca8ab8a1e69d86531727a36a0ff69
Max Emerson <maxemerson2013@gmail.com>
2024-02-29 16:10:27 +0000

    form mostly working

--- week8project/src/app/music/addsong/page.js
+++ week8project/src/app/music/addsong/page.js
@@ -11,8 +11,11 @@ export default function AddSong() {
     const name = formData.get("name");
     const artist = formData.get("artist");
 
-    await sql`INSERT INTO music (title, artist, year, name) VALUES (${title}, ${artist}, ${year}, ${name})`;
-    // await sql`INSERT INTO genre (name) VALUES (${name}) WHERE id = guest.id`;
+    await sql`INSERT INTO music (title, year, artist) VALUES (${title}, ${year}, ${artist})`;
+    await sql`INSERT INTO genre (genre) VALUES (${name})`;
+    const data =
+      await sql`SELECT music.id, music.artist, music.year, genre.genre FROM music INNER JOIN genre ON music.id = genre_id`;
+    data.rows;
 
     revalidatePath("/music");
     redirect("/music");
@@ -23,10 +26,10 @@ export default function AddSong() {
       <form action={handleAddSong}>
         <label>Song title</label>
         <input name="title" placeholder="Enter song title" />
-        <label>Artist</label>
-        <input name="artist" placeholder="Enter song artist" />
         <label> Year of release</label>
         <input name="year" placeholder="Enter song release year" />
+        <label>Artist</label>
+        <input name="artist" placeholder="Enter song artist" />
         <label>Genre</label>
         <input name="name" placeholder="Enter genre of song" />
         <SubmitButton />
--- week8project/src/app/music/page.js
+++ week8project/src/app/music/page.js
@@ -1,18 +1,17 @@
 import { sql } from "@vercel/postgres";
 
 export default async function music() {
-  let music = (
-    await sql`SELECT music.id, music.artist, music.year, genre.name FROM music INNER JOIN genre ON music.id = genre.id`
-  ).rows;
+  let music = (await sql`SELECT * from music`).rows;
   return (
     <div>
       <h1>Here is a list of music:</h1>
       {music.map(() => {
         <div key={music.id}>
           <h2>Song title: {music.title}</h2>
-          <h2>Artist: {music.artist}</h2>
           <h2>Year of release: {music.year}</h2>
-          <p>Genre: {music.name}</p>
+          <h2>Artist: {music.artist}</h2>
+          <p>Genre: {music.genre}</p>
+          console.log({music.genre})
         </div>;
       })}
     </div>
