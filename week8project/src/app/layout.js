35c4f8d662c5b8108201483f5089c3ad493962a9
Max Emerson <maxemerson2013@gmail.com>
2024-03-04 13:24:57 +0000

    added sign in functionality

--- week8project/src/app/layout.js
+++ week8project/src/app/layout.js
@@ -1,6 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { ClerkProvider } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
+import { authMiddleware } from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,17 +14,20 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const { userId } = authMiddleware();
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <nav className="p-2 m-2 bg-red-800 text-white space-x-14 h-12 text-lg">
-          <Link href="/">Home page</Link>
-          <Link href="/music">Find your tunes</Link>
-          <Link href="/music/addsong">Add a song here</Link>
-        </nav>
-
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <nav className="p-2 m-2 bg-red-800 text-white space-x-14 h-12 text-lg">
+            <Link href="/">Home page</Link>
+            <Link href="/music">Find your tunes</Link>
+            <Link href="/music/addsong">Add a song here</Link>
+            {userId ? <UserButton /> : <SignInButton />}
+          </nav>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
